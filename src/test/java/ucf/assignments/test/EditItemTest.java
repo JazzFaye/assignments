656e373212bb26702dc3aa773f18daa71bb28c45
JazzFaye <jazzfaye@knights.ucf.edu>
2021-10-31 21:51:50 -0400

    Pseudocode for test created

---
+++ src/test/java/ucf/assignments/test/EditItemTest.java
@@ -0,0 +1,32 @@
+package ucf.assignments.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EditItemTest {
+
+    @Test
+    void edit_Status() {
+        //Create a new Item
+        //Obtain Status
+        //Assign a status
+        //Assert that the expected status is equal to the actual status
+    }
+
+    @Test
+    void edit_Desc() {
+        //Create new Item
+        //Obtain Description
+        //Assign a description
+        //Assert that the expected description is equal to the actual description
+    }
+
+    @Test
+    void edit_DueDate() {
+        //Create new Item
+        //Obtain due date
+        //Assign a due date
+        //Assert that the expected due date is equal to the actual due date
+    }
+}
---
+++ src/test/java/ucf/assignments/test/ListTest.java
@@ -0,0 +1,42 @@
+package ucf.assignments.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListTest {
+
+    @Test
+    void add_List() {
+        //Create new list
+        //Check if new list is added
+    }
+
+    @Test
+    void remove_List() {
+        //Create new list
+        //Remove list
+        //Check if list is removed
+    }
+
+    @Test
+    void Save_List() {
+        //Create new list
+        //Save list
+        //Create new file path
+        //AssertTrue(file.exists())
+        //Delete file
+
+    }
+    @Test
+    void Load_List(){
+        //Create new list
+        //save list
+        //Create new file path
+        //Load list
+        //AssertTrue(file.exists())
+        //Delete file
+
+
+    }
+}
